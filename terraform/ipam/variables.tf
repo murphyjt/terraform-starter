d3c489dd13fbc87d919d1a7dcb2af013b0aa50d2
Joshua Murphy <joshua.murphy@interwellhealth.com>
2024-10-15 16:16:30 -0500

    Use terraform{}.cloud block

--- terraform/ipam/variables.tf
+++ terraform/ipam/variables.tf
@@ -3,14 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
-
-variable "tfc_organization_name" {
-  type        = string
-  description = "The name of your Terraform Cloud organization"
-}
-
-variable "tfc_workspace_name" {
-  type        = string
-  default     = "my-ipam-workspace"
-  description = "The name of the workspace that you'd like to create and connect to AWS"
-}
--- terraform/ipam/versions.tf
+++ terraform/ipam/versions.tf
@@ -1,18 +1,14 @@
 terraform {
-  required_version = ">= 1.9.7"
+  cloud {
+    workspaces {
+      tags = ["networking"]
+    }
+  }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.71"
-    }
-  }
-
-  backend "remote" {
-    organization = var.tfc_organization_name
-
-    workspaces {
-      name = var.tfc_workspace_name
     }
   }
 }
